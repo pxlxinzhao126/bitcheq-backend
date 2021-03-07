0344a30107159f53e0b215a067cdf9190b2ed7c4
patrick pu <pxlxinzhao126@gmail.com>
2021-03-07 08:01:14 -0500

    login works

---
+++ src/app.controller.ts
@@ -0,0 +1,19 @@
+import { Controller, Get, Post, UseGuards, Request } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { AppService } from './app.service';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @UseGuards(AuthGuard('local'))
+  @Post('auth/login')
+  async login(@Request() req) {
+    return req.user;
+  }
+
+  @Get()
+  getHello(): string {
+    return this.appService.getHello();
+  }
+}
