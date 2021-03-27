67d790b1b38f7add39fffc625e09430de4812372
patrick pu <pxlxinzhao@gmail.com>
2021-03-27 10:56:24 -0400

    clean up withdraw comment

--- src/block/block.controller.ts
+++ src/block/block.controller.ts
@@ -29,19 +29,6 @@ export class BlockController {
     throw new HttpException('address is required', HttpStatus.BAD_REQUEST);
   }
 
-  /**
-   * {
-      status: 'success',
-      data: {
-        network: 'BTCTEST',
-        txid: '6d0b41befd9dbbbc7f43c710b6227aefd8cc45ab935c25350b40d96526c153ff',
-        amount_withdrawn: '0.00028055',
-        amount_sent: '0.00017600',
-        network_fee: '0.00010455',
-        blockio_fee: '0.00000000'
-      }
-    }
-   */
   @Post('withdraw')
   async withdraw(@Body() withdraw_data) {
     const { username, amount, toAddress } = withdraw_data;
@@ -64,9 +51,7 @@ export class BlockController {
   }
 
   /**
-   * To open up webhook endpoint to internet: ngrok http -host-header=rewrite localhost:3000
-   * this.block.create_notification({ type: 'account', url: 'http://898191d27b2f.ngrok.io/block/webhook' });
-   * block_io.delete_notification({ notification_id: 'NOTIFICATION ID' });
+   * To open up local webhook endpoint to internet: ngrok http -host-header=rewrite localhost:3000
    */
   @Post('webhook')
   async handleWebhook(@Body() webhook_response) {
